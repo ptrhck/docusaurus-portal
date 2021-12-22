72032d66d6d5f197a75cd492a5b39f75b3f5df18
sebastienlorber <lorber.sebastien@gmail.com>
2021-12-22 19:59:11 +0100

    fix broken links

--- android/docusaurus.config.js
+++ android/docusaurus.config.js
@@ -77,8 +77,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Docs',
+                to: '/',
               },
             ],
           },
